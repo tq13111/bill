4b0636a34a408e9c7494e6c09da9b3d8d24df4a0
TQ <756342530@qq.com>
2021-01-21 23:55:45 +0800

    引入 svg,添加 Icon 组件

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,14 +1,22 @@
 <template>
   <div>
-    <router-link to="/Label">标签</router-link>
-    <router-link to="/Money">记账</router-link>
-    <router-link to="/Statistics">统计</router-link>
+    <router-link to="/Label">
+      <Icon name="label"/>
+      标签</router-link>
+    <router-link to="/Money">
+      <Icon name="money"/>
+      记账</router-link>
+    <router-link to="/statistics">
+      <Icon name="statistics"/>
+      统计</router-link>
   </div>
 </template>
-
 <script lang="ts">
+import Icon from '@/components/Icon.vue';
+
 export default {
-  name: 'Nav.vue'
+  name: 'Nav.vue',
+  components: {Icon}
 };
 </script>
 
