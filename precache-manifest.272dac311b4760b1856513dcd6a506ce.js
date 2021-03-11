a0af045b0a644b7d2b0f98a356c088d5724e8828
TQ <756342530@qq.com>
2021-03-11 21:57:02 +0800

    deploy

---
+++ precache-manifest.272dac311b4760b1856513dcd6a506ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c2c823a2c94625d872",
+    "url": "/muji-bill/css/app.5aafc680.css"
+  },
+  {
+    "revision": "a6f6ecb247d57f0fe3a718bde7ebe16d",
+    "url": "/muji-bill/index.html"
+  },
+  {
+    "revision": "36c2c823a2c94625d872",
+    "url": "/muji-bill/js/app.3a03db6e.js"
+  },
+  {
+    "revision": "b1dec495fe0c16ed8a62",
+    "url": "/muji-bill/js/chunk-vendors.510bcb25.js"
+  },
+  {
+    "revision": "bb02a9a561dfff32ddd679da58e39c0d",
+    "url": "/muji-bill/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/muji-bill/robots.txt"
+  }
+]);
